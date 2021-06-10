cec1b2c67d7255426ecfefecddc09b9601789a30
martin.grofcik <mgrofcik@gmail.com>
2021-06-10 21:28:29 +0200

    flowable-admin app sample

---
+++ crp-flowable-admin/src/main/java/org/crp/flowable/admin/CrpFlowableAdminApplication.java
@@ -0,0 +1,13 @@
+package org.crp.flowable.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrpFlowableAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrpFlowableAdminApplication.class, args);
+	}
+
+}
---
+++ crp-flowable-admin/src/test/java/org/crp/flowable/admin/CrpFlowableAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crp.flowable.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrpFlowableAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
